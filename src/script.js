d365fec6a6a35b00d511ec2d6966e832834d1a89
Macaulay Suad Esinam <suadmacaulay50@gmail.com>
2025-05-01 14:59:36 +0000

    Update script.js

--- src/script.js
+++ src/script.js
@@ -87,6 +87,7 @@ userForm.addEventListener("submit", (e) => {
 
   const transformedName = nameInput
     .split(" ")
+    .filter((word) => word.trim() !== "")
     .map((word) => {
       return word[0].toUpperCase() + word.slice(1).toLowerCase();
     })
